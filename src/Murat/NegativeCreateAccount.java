781b7270356ef307b1d303c602086e89a3d521a1
murat <geomury48@gmail.com>
2023-08-30 21:39:07 +0200

    Negative  assert yapildi

--- src/Murat/NegativeCreateAccount.java
+++ src/Murat/NegativeCreateAccount.java
@@ -2,6 +2,7 @@ package Murat;
 
 import Utlity.BaseDriver;
 import Utlity.MyFunc;
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -35,12 +36,15 @@ public class NegativeCreateAccount extends BaseDriver {
         WebElement registerBtn=driver.findElement(By.id("register-button"));
         registerBtn.click();
         MyFunc.Bekle(2);
+        WebElement msj=driver.findElement(By.xpath("//*[text()='The specified email already exists']"));
+
+        Assert.assertTrue(msj.getText().contains("The specified email already exists"));
 
         //'The specified email already exists'
 
 
 
-        BekleVeKapat();
+       // BekleVeKapat();
 
 
 
