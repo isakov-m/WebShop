b15af201351efe18093e53aa9db03d2078fcf3c8
PC <onur2220@gmail.com>
2023-08-31 21:12:24 +0200

    negatif login ve sayfa oylama testi yüklendi + pass generator olusturuldu

---
+++ src/Utlity/passgenarator.java
@@ -0,0 +1,19 @@
+package Utlity;
+
+import java.util.Random;
+
+public class passgenarator {
+    public static String generateRandomPassword(int length) {
+        Random random = new Random();
+        StringBuilder password = new StringBuilder(length);
+        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789.@-_*!#?$";
+
+        for (int i = 0; i < length; i++) {
+            int randomIndex = random.nextInt(characters.length());
+            char randomChar = characters.charAt(randomIndex);
+            password.append(randomChar);
+        }
+
+        return password.toString();
+    }
+}
